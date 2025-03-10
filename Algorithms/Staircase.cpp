ced01fa71e95743589675815a093f703538094fa
Abin <bintanghidayatullah021@gmail.com>
2025-03-10 09:51:01 +0700

    added comments

--- Algorithms/Staircase.cpp
+++ Algorithms/Staircase.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
 using namespace std;
 
-
+// Function to print a staircase pattern of height n
 void staircase(int n) {
+   // Loop through each level of the staircase
    for (int i = 0; i < n; i++) {
+       // Loop through each position in the current level
        for (int j = 0; j < n; j++) {
-           if (j < n-1-i) {
-               cout << " ";
+           // Print a space if the current position is before the '#' characters
+           if (j < n - 1 - i) {
+               cout << " "; // Print space for alignment
            } else {
-               cout << "#";
+               cout << "#"; // Print '#' to form the staircase
            }
        }
-       cout << "" << endl;
+       cout << "" << endl; // Move to the next line after finishing the current level
    }
 }
 
-
 int main() {
-   int inp;
-   cin >> inp;
-   staircase(inp);
+   int inp; // Variable to hold the input height of the staircase
+   cin >> inp; // Read the height of the staircase from user input
+   staircase(inp); // Call the staircase function to print the pattern
 }
