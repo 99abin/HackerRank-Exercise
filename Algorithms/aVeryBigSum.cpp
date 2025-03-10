7eac2746fef492d66607d50ccf946f571f7896b1
Abin <bintanghidayatullah021@gmail.com>
2025-03-10 09:48:53 +0700

    added comments

--- Algorithms/aVeryBigSum.cpp
+++ Algorithms/aVeryBigSum.cpp
@@ -2,22 +2,21 @@
 #include <vector>
 #include <numeric>
 
-
 using namespace std;
 
-
+// Function to calculate the sum of a vector of long integers
 long aVeryBigSum(vector<long> ar) {
+   // Use accumulate to sum all elements in the vector, starting from 0L
    return accumulate(ar.begin(), ar.end(), 0L);
 }
 
-
 int main() {
-   vector<long> inp;
-   int n, temp;
-   cin >> n;
+   vector<long> inp; // Vector to store input numbers
+   int n, temp; // Variables to hold the number of elements and temporary input
+   cin >> n; // Read the number of elements
    for (int i = 0; i < n; i++) {
-       cin >> temp;
-       inp.push_back(temp);
+       cin >> temp; // Read each element
+       inp.push_back(temp); // Add the element to the vector
    }
-   cout << aVeryBigSum(inp);
+   cout << aVeryBigSum(inp); // Output the sum of the elements in the vector
 }
