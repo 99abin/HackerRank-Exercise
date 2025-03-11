787aef5eff21a717bdd06cc978ecc50f12da68be
Abin <bintanghidayatullah021@gmail.com>
2025-03-11 09:53:31 +0700

    Create UtopianTree.cpp

---
+++ Algorithms/UtopianTree.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int utopianTree(int n) {
+    int height = 1;
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 == 0) {
+          height += 1;
+        } else {
+          height *= 2;
+        }
+    }
+    return height;
+}
+
+int main() {
+  int tc, temp;
+  cin >> tc;
+  for (int i = 0; i < tc; i++) {
+    cin >> temp;
+    cout << utopianTree(temp) << endl;
+  }  
+}
