587fc264955be35ba8600e40e783e55e8e48cf4a
Abin <bintanghidayatullah021@gmail.com>
2025-03-16 10:51:50 +0700

    simplified code

--- Algorithms/PickingNumbers.cpp
+++ Algorithms/PickingNumbers.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 int pickingNumbers(vector<int> a) {
-    vector<int> result;
+    int greatest = 0, result;
     for (int i = 0; i < a.size(); i++) {
         int count1 = 1, count2 = 1;
         for (int j = i + 1; j < a.size(); j ++) {
             count1 += (a[i] - a[j] == 1 || a[i] - a[j] == 0);
             count2 += (a[i] - a[j] == -1 || a[i] - a[j] == 0);
-            if (count1 > count2) {
-                result.push_back(count1);
-            } else {
-                result.push_back(count2);
+            if (count1 >= count2 && count1 > greatest) {
+                result = count1;
+                greatest = count1;
+            } else if (count2 > count1 && count2 > greatest) {
+                result = count2;
+                greatest = count2;
             }
         }
     }
-    sort(result.begin(), result.end());
-    return result.back();
+    return result;
 }
 
 int main() {
